fb44d47d4e0f3aad4fb92cfc6d72e07d0c5cef37
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-10-17 03:03:03 +0300

    fix

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,40 +1,40 @@
-create database if not exists alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-use alx_book_store;
+USE alx_book_store;
 
-create table Books (
-    book_id int primary key auto_increment,
-    title varchar(130),
-    author_id int not null,
-    price Double(10,2),
-    published_date date,
-    foreign key (author_id) references Authors(author_id)
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130),
+    author_id INT NOT NULL,
+    price DOUBLE(10,2),
+    published_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-create table Authors (
-    author_id int primary key auto_increment,
-    author_name varchar(215)
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215)
 );
 
-create table Customers (
-    customer_id int primary key auto_increment,
-    customer_name varchar(215),
-    email varchar(255) unique,
-    address text
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215),
+    email VARCHAR(255) UNIQUE,
+    address TEXT
 );
 
-create table Orders (
-    order_id int primary key auto_increment,
-    customer_id int not null,
-    order_date date,
-    foreign key (customer_id) references Customers(customer_id)
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-create table Order_Details (
-    orderdetailid int primary key auto_increment,
-    order_id int not null,
-    book_id int not null,
-    quantity Double(10,2),
-    foreign key (order_id) references Orders(order_id),
-    foreign key (book_id) references Books(book_id)
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE(10,2),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
